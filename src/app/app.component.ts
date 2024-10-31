e26f5371ebcb06ec15aa2efb49f799e49e6f48fe
Dathan Cruz <designers_crux@live.com>
2024-10-30 23:16:11 -0400

    Router changes to the app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AboutComponent } from "./features/about/about.component";
-import { CertificationsComponent } from "./features/certifications/certifications.component";
-import { MenuComponent } from "./features/menu/menu.component";
-import { TestimonialsComponent } from "./features/testimonials/testimonials.component";
-import { ContactFormComponent } from "./features/contact-form/contact-form.component";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent, CertificationsComponent, MenuComponent, TestimonialsComponent, ContactFormComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'misslizkitchen-landing-page';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,54 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { 
-        path: "about-miss-lizzs-kitchen",
-        loadComponent: () => import('./features/about/about.component').then(c => c.AboutComponent),
-    },
-    {
-        path: "certifications",
-        loadComponent: () => import('./features/certifications/certifications.component').then(c => c.CertificationsComponent),
-    },
-    {
-        path: "menu",
-        loadComponent: () => import('./features/menu/menu.component').then(c => c.MenuComponent),
-    },
-    {
-        path: "app-coming-soon",
-        loadComponent: () => import('./features/food-truck-app/food-truck-app.component').then(c => c.FoodTruckAppComponent),
-    },
-    {
-        path: "testimonials",
-        loadComponent: () => import('./features/testimonials/testimonials.component').then(c => c.TestimonialsComponent),
-    },
-    {
-        path: "**",
-        loadComponent: () => import('./features/page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent),
-    }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    title: 'home',
+    loadComponent: () =>
+      import('./features/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'about-miss-lizzs-kitchen',
+    title: 'About Miss Lizzs Kitchen',
+    loadComponent: () =>
+      import('./features/about/about.component').then((c) => c.AboutComponent),
+  },
+  {
+    path: 'certifications',
+    title: 'Certifications',
+    loadComponent: () =>
+      import('./features/certifications/certifications.component').then(
+        (c) => c.CertificationsComponent
+      ),
+  },
+  {
+    path: 'menu',
+    title: 'Menu',
+    loadComponent: () =>
+      import('./features/menu/menu.component').then((c) => c.MenuComponent),
+  },
+  {
+    path: 'app-coming-soon',
+    title: 'App Coming Soon',
+    loadComponent: () =>
+      import('./features/food-truck-app/food-truck-app.component').then(
+        (c) => c.FoodTruckAppComponent
+      ),
+  },
+  {
+    path: 'testimonials',
+    title: 'Testimonials',
+    loadComponent: () =>
+      import('./features/testimonials/testimonials.component').then(
+        (c) => c.TestimonialsComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./features/page-not-found/page-not-found.component').then(
+        (c) => c.PageNotFoundComponent
+      ),
+  },
 ];
